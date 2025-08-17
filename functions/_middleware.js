7f7c01f5d4b9a298220bcbe7573670e49c4e6f3b
A827 <coskuntunckaya@gmail.com>
2025-08-17 17:52:29 +0000

    Add error handling middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,22 +1,11 @@
-export async function onRequest(context, next) {
-  const res = await next();
-
-  // CORS (you can restrict “*” to your domain later)
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "content-type");
-
-  // Pretty JSON if body is an object already
-  return res;
+// functions/_middleware.js
+export async function onRequest(context) {
+  try {
+    return await context.next();
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ ok:false, error:String(err) }),
+      { status:500, headers:{ "content-type":"application/json" } }
+    );
+  }
 }
-
-export const onRequestOptions = () =>
-  new Response(null, {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "content-type",
-      "Access-Control-Max-Age": "86400",
-    },
-  });
